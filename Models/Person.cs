7eadc02a6b1a95858a104baa95f5844ef34287cf
eddychen101020@gmail.com = eddychen86 <eddychen101020@gmail.com>
2025-07-09 22:44:25 +0800

    update: Add "Code First" DB info and config.

---
+++ Models/Person.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace Matrix.Models;
+
+public class Person
+{
+  [Key]
+  public required string PersonId { get; set; }
+
+  public int Role { get; set; } = 0;
+  public int Status { get; set; } = 0;
+  // public required DateTime CreateTime { get; set; }
+  [MaxLength(50)]
+  public string? DisplayName { get; set; }
+  [MaxLength(300)]
+  public string? Bio { get; set; }
+  [MaxLength(2048)]
+  public string? AvatarPath { get; set; }
+  [MaxLength(2048)]
+  public string? BannerPath { get; set; }
+  [MaxLength(2048)]
+  public string? ExternalUrl { get; set; }
+  public int IsPrivate { get; set; } = 0;
+  public string? WalletAddress { get; set; }
+  public DateTime? ModifyTime { get; set; }
+
+  // Navigation properties
+  [ForeignKey("PersonId")]
+  public virtual required IdentityUser User { get; set; }
+  public virtual ICollection<Article> Articles { get; set; } = new List<Article>();
+  public virtual ICollection<Reply> Replies { get; set; } = new List<Reply>();
+  public virtual ICollection<PraiseCollect> PraiseCollects { get; set; } = new List<PraiseCollect>();
+  public virtual ICollection<Follow> Follows { get; set; } = new List<Follow>();
+  public virtual ICollection<Notification> NotificationsSent { get; set; } = new List<Notification>();
+  public virtual ICollection<Notification> NotificationsReceived { get; set; } = new List<Notification>();
+  public virtual ICollection<Report> ReportsMade { get; set; } = new List<Report>();
+  public virtual ICollection<Report> ReportsResolved { get; set; } = new List<Report>();
+  public virtual ICollection<LoginRecord> LoginRecords { get; set; } = new List<LoginRecord>();
+}
