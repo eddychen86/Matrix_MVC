5fc6e8ec084f13c309cc01195865f568efa28341
EasonTsia <561993111e@gmail.com>
2025-07-09 14:05:30 +0800

    更新:把原本寫錯的地方的程式碼拉出來

---
+++ Controllers/CollectController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Matrix.Controllers
+{
+    public class CollectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET:Collect
+        [HttpGet]
+        public IActionResult Collect()
+        {
+            return View();
+        }
+    }
+}
