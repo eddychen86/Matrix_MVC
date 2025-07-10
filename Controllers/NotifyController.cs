8d1b95d6426c3161b158db98486f70e9e6bac6b8
EasonTsia <561993111e@gmail.com>
2025-07-11 00:40:07 +0800

    style:製作Notify頁面

---
+++ Controllers/NotifyController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Matrix.Controllers
+{
+    public class NotifyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET:Notify
+        [HttpGet]
+        public IActionResult Notify()
+        {
+            return View();
+        }
+    }
+}
