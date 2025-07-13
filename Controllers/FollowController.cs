c4415e77921dd032b11d45cdd4585acec39dc80a
EasonTsia <561993111e@gmail.com>
2025-07-13 13:18:49 +0800

    style:編輯Follow網頁畫面

---
+++ Controllers/FollowController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Matrix.Controllers
+{
+    public class FollowController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Follow()
+        {
+            return View();
+        }
+    }
+}
