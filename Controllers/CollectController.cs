39568e36875d696301f68abdaea87fbfdacd2c86
EasonTsia <561993111e@gmail.com>
2025-07-22 12:05:32 +0800

    style:修改scss

--- Controllers/CollectController.cs
+++ Controllers/CollectController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Matrix.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Matrix.Controllers
 {
@@ -13,7 +14,33 @@ namespace Matrix.Controllers
         [HttpGet]
         public IActionResult Collect()
         {
-            return View();
+            // 寫死的假資料
+            var fakeData = new List<CollectItemViewModel>
+            {
+                new CollectItemViewModel
+                {
+                    Title = "未來科技圖像",
+                    ImageUrl = Url.Content("~/static/img/Cute.png"),
+                    AuthorName = "小西瓜",
+                    CollectedAt = DateTime.Now.AddDays(-1)
+                },
+                new CollectItemViewModel
+                {
+                    Title = "數位宇宙奇想",
+                    ImageUrl = Url.Content("~/static/img/Cute.png"),
+                    AuthorName = "大豬豬",
+                    CollectedAt = DateTime.Now.AddDays(-2)
+                },
+                new CollectItemViewModel
+                {
+                    Title = "區塊鏈紀實",
+                    ImageUrl = Url.Content("~/static/img/Cute.png"),
+                    AuthorName = "笑笑貓",
+                    CollectedAt = DateTime.Now.AddDays(-3)
+                }
+            };
+
+            return View(fakeData); // 把資料傳給 Collect.cshtml
         }
     }
 }
---
+++ ViewModels/CollectItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Matrix.ViewModels
+{
+    public class CollectItemViewModel
+    {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string AuthorName { get; set; }
+        public DateTime CollectedAt { get; set; }
+    }
+}
