1c69d5a8e9994aef9b84edd919923d9eb2bba708
eddychen101020@gmail.com = eddychen86 <eddychen101020@gmail.com>
2025-07-25 04:15:50 +0800

    feat: Add popup data API endpoint and enhance popup component with improved styling and functionality

--- Controllers/CollectController.cs
+++ Controllers/CollectController.cs
@@ -17,23 +17,44 @@ namespace Matrix.Controllers
             // 寫死的假資料
             var fakeData = new List<CollectItemViewModel>
             {
-                new CollectItemViewModel
+                new()
                 {
-                    Title = "未來科技圖像",
+                    Title = "test 00",
                     ImageUrl = Url.Content("~/static/img/Cute.png"),
                     AuthorName = "小西瓜",
                     CollectedAt = DateTime.Now.AddDays(-1)
                 },
-                new CollectItemViewModel
+                new()
                 {
-                    Title = "數位宇宙奇想",
+                    Title = "test 01",
                     ImageUrl = Url.Content("~/static/img/Cute.png"),
                     AuthorName = "大豬豬",
                     CollectedAt = DateTime.Now.AddDays(-2)
                 },
-                new CollectItemViewModel
+                new()
                 {
-                    Title = "區塊鏈紀實",
+                    Title = "test 02",
+                    ImageUrl = Url.Content("~/static/img/Cute.png"),
+                    AuthorName = "笑笑貓",
+                    CollectedAt = DateTime.Now.AddDays(-3)
+                },
+                new()
+                {
+                    Title = "test 03",
+                    ImageUrl = Url.Content("~/static/img/Cute.png"),
+                    AuthorName = "小西瓜",
+                    CollectedAt = DateTime.Now.AddDays(-1)
+                },
+                new()
+                {
+                    Title = "test 04",
+                    ImageUrl = Url.Content("~/static/img/Cute.png"),
+                    AuthorName = "大豬豬",
+                    CollectedAt = DateTime.Now.AddDays(-2)
+                },
+                new()
+                {
+                    Title = "test 05",
                     ImageUrl = Url.Content("~/static/img/Cute.png"),
                     AuthorName = "笑笑貓",
                     CollectedAt = DateTime.Now.AddDays(-3)
@@ -41,6 +62,61 @@ namespace Matrix.Controllers
             };
 
             return View(fakeData); // 把資料傳給 Collect.cshtml
+        }
+
+        // API endpoint for popup data
+        [HttpGet]
+        [Route("/api/collects")]
+        public IActionResult GetCollectsData()
+        {
+            // 同樣的假資料，但返回 JSON
+            var fakeData = new List<CollectItemViewModel>
+            {
+                new()
+                {
+                    Title = "test 00",
+                    ImageUrl = Url.Content("~/static/img/Cute.png"),
+                    AuthorName = "小西瓜",
+                    CollectedAt = DateTime.Now.AddDays(-1)
+                },
+                new()
+                {
+                    Title = "test 01",
+                    ImageUrl = Url.Content("~/static/img/Cute.png"),
+                    AuthorName = "大豬豬",
+                    CollectedAt = DateTime.Now.AddDays(-2)
+                },
+                new()
+                {
+                    Title = "test 02",
+                    ImageUrl = Url.Content("~/static/img/Cute.png"),
+                    AuthorName = "笑笑貓",
+                    CollectedAt = DateTime.Now.AddDays(-3)
+                },
+                new()
+                {
+                    Title = "test 03",
+                    ImageUrl = Url.Content("~/static/img/Cute.png"),
+                    AuthorName = "小西瓜",
+                    CollectedAt = DateTime.Now.AddDays(-1)
+                },
+                new()
+                {
+                    Title = "test 04",
+                    ImageUrl = Url.Content("~/static/img/Cute.png"),
+                    AuthorName = "大豬豬",
+                    CollectedAt = DateTime.Now.AddDays(-2)
+                },
+                new()
+                {
+                    Title = "test 05",
+                    ImageUrl = Url.Content("~/static/img/Cute.png"),
+                    AuthorName = "笑笑貓",
+                    CollectedAt = DateTime.Now.AddDays(-3)
+                }
+            };
+
+            return Json(fakeData); // 返回 JSON 格式
         }
     }
 }
