f1831bbddbd74c3a173d8931dc91a58da05a27f4
EasonTsia <561993111e@gmail.com>
2025-07-27 20:00:31 +0800

    refactor:連接資料庫資料

--- ViewModels/CollectItemViewModel.cs
+++ ViewModels/CollectItemViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class CollectItemViewModel
     {
-        public Guid Id { get; set; }
+        //public Guid Id { get; set; }
         public string Title { get; set; } = null!;
         public string ImageUrl { get; set; } = null!;
         public string AuthorName { get; set; } = null!;
