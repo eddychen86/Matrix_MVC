c35507f71d202600ecaee6d01652a20dc3f6e6b1
EasonTsia <561993111e@gmail.com>
2025-07-29 10:49:45 +0800

    refactor:Notify頁面與資料庫連接

--- Controllers/NotifyController.cs
+++ Controllers/NotifyController.cs
@@ -1,41 +1,52 @@
-﻿using Matrix.ViewModels;
+﻿using Matrix.Data;
+using Matrix.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Matrix.Controllers
 {
     public class NotifyController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public NotifyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
+
         //GET:Notify
         [HttpGet]
-        public IActionResult Notify()
+        [Route("/api/notify")]
+        public IActionResult GetNotifyData()
         {
-            var fakeNotify = new List<NotifyItemViewModel>
-        {
-            new NotifyItemViewModel {
-                SenderName = "Eddy",
-                SenderAvatarUrl = "~/static/img/Cute.png",
-                Message = "Great artwork, love it!",
-                TimeAgo = "2h"
-            },
-            new NotifyItemViewModel {
-                SenderName = "Eason",
-                SenderAvatarUrl = "~/static/img/Cute.png",
-                Message = "Awesome job! Keep going!",
-                TimeAgo = "5h"
-            },
-            new NotifyItemViewModel {
-                SenderName = "Hung",
-                SenderAvatarUrl = "~/static/img/Cute.png",
-                Message = "Can I share this?",
-                TimeAgo = "1d"
-            }
-        };
-            return View("Notify",fakeNotify);
+            // TODO: 未來改為從登入者取得 PersonId，例如 User.Identity.Name → User → Person.Id
+            var currentUserId = Guid.Parse("7ffbe594-de54-452a-92b0-311631587369");
+
+            var notifications = _context.Notifications
+                .Where(n => n.GetId == currentUserId)
+                .Include(n => n.Sender) // 載入 Sender 的 Person 對象
+                .OrderByDescending(n => n.SentTime)
+                .Take(10)
+                .ToList();
+
+
+            var notifyList = notifications.Select(n => new NotifyItemViewModel
+            { 
+                SenderName = n.Sender.DisplayName ?? "匿名",
+                SenderAvatarUrl = n.Sender.AvatarPath ?? "/static/img/default-avatar.png",
+                Message = n.Type == 0 ?"有人留言了你的貼文"
+                          : n.Type == 1 ? "有人私訊你"
+                          :"有新通知",
+                SentTime = n.SentTime
+            }).ToList();
+
+            return Json(notifyList);
+
         }
     }
 }
--- ViewModels/NotifyItemViewModel.cs
+++ ViewModels/NotifyItemViewModel.cs
@@ -5,6 +5,6 @@
         public string SenderName { get; set; }
         public string SenderAvatarUrl { get; set; }
         public string Message { get; set; }
-        public string TimeAgo { get; set; }
+        public DateTime SentTime { get; set; }
     }
 }
