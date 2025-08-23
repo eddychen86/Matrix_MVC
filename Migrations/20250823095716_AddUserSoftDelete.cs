912300dfd2301cd9b996fff76c32f109183982f4
eddychen101020@gmail.com = eddychen86 <eddychen101020@gmail.com>
2025-08-23 17:58:03 +0800

    feat: add migration data

---
+++ Migrations/20250823095716_AddUserSoftDelete.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Matrix.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserSoftDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IsDelete",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                table: "Users");
+        }
+    }
+}
